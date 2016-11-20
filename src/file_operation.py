ba3bab56adfc4060ac66173b34e7e1c2affac007
Tanaka Hayate <pinqphayate@gmail.com>
2016-11-20 14:09:29 +0900

    add module to get product metrics

--- src/file_operation.py
+++ src/file_operation.py
@@ -11,6 +11,17 @@ def fild_all_files(directory):
             if isJava is not None :
                 list.append(module.Module(full_path))
     return list
+def get_all_filesa_text(directory):
+    list = []
+    for root, dirs, files in os.walk(directory):
+        for file_ in files:
+            full_path = os.path.join(root, file_)
+            # isJava = re.search(full_path, r'(\.java)$')
+            isJava = re.search("\.JAVA$",full_path.upper())
+            if isJava is not None :
+                list.append(module.Module(full_path))
+    return list
+
 
 root = '/Users/phayate/src/ApacheDerby/'
 ver = '10.12'
--- src/module.py
+++ src/module.py
@@ -2,6 +2,7 @@ class Module(object):
     def __init__(self, filename):
          self.filename = filename
          self.LOC = int()
+         self.TChar = int()
          self.churnMetrics = float()
          self.relativeChrun = float()
          self.DeletredChurn = float()
